7f89cd3edc658cf492e6c4a1e0373539c6925fcb
DenisED1 <denis.edelbroek@student.hu.nl>
2020-06-14 10:42:11 +0200

    wordtest

---
+++ src/test/java/lingo/lingogame/domain/WordTest.java
@@ -0,0 +1,43 @@
+package lingo.lingogame.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Word")
+public class WordTest {
+	private final int wordid = 1;
+	private final String wordstr = "koekje";
+	private final int length = 6;
+	private Word word;
+
+	private final int langid = 1;
+	private final String langstr = "Netherlands";
+	private Language language;
+
+	@BeforeEach
+	public void setUp() {
+		language = new Language(langid, langstr);
+		word = new Word(wordid, wordstr, length, language);
+	}
+
+	@Test
+	@DisplayName("gives the wordid")
+	public void getWordid() {
+		assertEquals(wordid, word.getWordid());
+	}
+
+	@Test
+	@DisplayName("gives the word")
+	public void getWord() {
+		assertEquals(word, word.getWord());
+	}
+
+	@Test
+	@DisplayName("gives length")
+	public void getLength() {
+		assertEquals(length, word.getLength());
+	}
+}
